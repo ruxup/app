8f48db1561fa0f400485e8946e3dd08e837c1387
radityasurya <radityasurya@hotmail.com>
2017-05-06 20:45:17 +0200

    add login form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the Login page.
@@ -13,8 +14,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  private todo: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.todo = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: [''],
+    });
+  }
+
+  logForm(){
+    console.log(this.todo.value)
   }
 
   ionViewDidLoad() {
