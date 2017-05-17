37231546ef2a09140724724bcfee75199dbdd1bb
radityasurya <radityasurya@hotmail.com>
2017-05-18 00:00:27 +0200

    add slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ViewChild, Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,9 +9,23 @@ import { IonicPage } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngAfterViewInit() {
+    this.initSlider();
+
+  }
+
+  initSlider() {
+    this.slides.pager = true;
+    this.slides.loop = true;
+    this.slides.speed = 500;
+    this.slides.autoplay = 8000;
+    // this.slides.effect = "fade";
   }
 
 }
